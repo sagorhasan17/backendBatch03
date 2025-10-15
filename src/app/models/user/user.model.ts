b17c11ea81d06c52129efaf428f162c1af821823
Sagor <sagor.codealoy@gmail.com>
2025-10-16 00:12:59 +0600

    file for auth

--- src/app/models/user/user.model.ts
+++ src/app/models/user/user.model.ts
@@ -1,1 +1,16 @@
  
+import { Schema, model } from 'mongoose';
+import type { IUser } from './user.interface.js';
+
+const userSchema = new Schema<IUser>({
+  userId: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isPasswordChanged: { type: Boolean, required: true, default: false },
+  role: { type: String, enum: ['admin', 'mentor', 'student'], required: true },
+  status: { type: String, enum: ['active', 'blocked', 'pending'], required: true, default: 'pending' },
+  isDeleted: { type: Boolean, required: true, default: false },
+  // createdAt: { type: Date, default: Date.now },
+  // updatedAt: { type: Date, default: Date.now },
+});
+
+export const UserModel = model<IUser>('User', userSchema);
